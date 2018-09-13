81e6d0a4c5205f26835d308509e02549de51c5a1
Clara <cg2984@nyu.edu>
2018-09-13 15:52:06 -0400

    Revised HW1 q3, ready to turn in

--- cg2984_hw1_q3.py
+++ cg2984_hw1_q3.py
@@ -3,15 +3,15 @@
 # 13 Sept. 2018
 # Homework 1
 
-quarters = int(input("Number of quarters"))
-dimes = int(input("Number of dimes"))
-nickels = int(input("Number of nickels"))
-pennies = int(input("Number of pennies"))
+quarters = int(input("Number of quarters ")) * 25
+dimes = int(input("Number of dimes ")) * 10 
+nickels = int(input("Number of nickels ")) * 5
+pennies = int(input("Number of pennies "))
 
 total_val = quarters + dimes + nickels + pennies
 dollar_val = total_val // 100
 cent_val = total_val - (dollar_val * 100)
 
-print("The total is " + dollar_val + " dollars and " + cent_val + " cents.")
+print("The total is " + str(dollar_val) + " dollar(s) and " + str(cent_val) + " cents.")
 
 
