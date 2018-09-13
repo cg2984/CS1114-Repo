8bd32031948f29c2aba896416c2df9a7905efd53
Clara <cg2984@nyu.edu>
2018-09-13 15:41:48 -0400

    HW1 question 5

---
+++ cg2984_hw1_q5.py
@@ -0,0 +1,12 @@
+# Clara Gilligan
+# CS - UY 1114
+# 13 Sept 2018
+# Homework 1
+
+radius = 1.3
+height = 4.83
+pi = 3.14
+volume = (pi * (r**2)) * h
+
+print("volume of a soda can in cubic inches: " + volume)
+
