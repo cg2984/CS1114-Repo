fe58ae4f02aec61f8c8128b7f2ba33f0b4d16d6e
cg2984 <31659760+cg2984@users.noreply.github.com>
2018-10-07 18:03:47 -0400

    Create cg2984_hw4_q3.py

---
+++ cg2984_hw4_q3.py
@@ -0,0 +1,14 @@
+#Clara Gilligan
+#CS 1114 
+#Homework 4
+#10-7-18
+
+n = int(input("Enter a positive integer: "))
+inital = 3
+multiples = 0
+factor = 0
+for i in range(0,n):
+  factor += 1
+  multiples = intital*factor
+  print(multiples)
+print("Done")
