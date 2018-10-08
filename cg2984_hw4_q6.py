1c54cb3e4acc4913eafc96bddb805b61f8f638cc
cg2984 <31659760+cg2984@users.noreply.github.com>
2018-10-07 20:39:18 -0400

    Create cg2984_hw4_q6.py

---
+++ cg2984_hw4_q6.py
@@ -0,0 +1,16 @@
+#Clara Gilligan
+#CS 1114
+#Homework 4
+#10-7-18
+
+import random
+randNum = random.randint(1,100)
+guessNum = int(input("Enter a number: ")
+while randNum != guessNum: 
+  if guessNum > randNum: 
+    print("The number was too high, go lower")
+    guessNum = int(input("Enter a number: ")
+  elif guessNum < randNum:
+    print("The number was too low, go higher")
+    guessNum = int(input("Enter a number: ")
+print("Congrats! You guessed the number!")
