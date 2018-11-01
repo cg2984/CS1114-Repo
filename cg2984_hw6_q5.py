280c55e236a03be910cb0abe19d8b6939bc155a7
cg2984 <31659760+cg2984@users.noreply.github.com>
2018-11-01 16:59:29 -0400

    Add files via upload

---
+++ cg2984_hw6_q5.py
@@ -0,0 +1,10 @@
+def sum_range(s, e):
+    total = s
+    for i in range(s+1,e+1):
+        total+=i
+    print(total)
+def main():
+    start = int(input("Enter a starting number: "))
+    end = int(input("Enter an ending number: "))
+    sum_range(start,end)
+main()
