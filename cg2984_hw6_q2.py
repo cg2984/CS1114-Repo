9abeeb3bbdccde0b6ebe85d9c524e53eca0aca57
cg2984 <31659760+cg2984@users.noreply.github.com>
2018-11-02 23:29:51 -0400

    Update cg2984_hw6_q2.py

--- cg2984_hw6_q2.py
+++ cg2984_hw6_q2.py
@@ -1,1 +1,21 @@
+def print_top(offset, width) :
+    # signature: int -> NoneType
+    print ((offset+(width//2))* ' ' + '^')
+def print_middle(offset, width) :
+    # signature: int , int -> NoneType
+    print (offset * ' ' + '/' + width * ' ' + '\\')
+def print_bottom(offset , width):
+    # signature: int , int -> NoneType
+    print ((offset)* ' ' + width * '-')
+def print_triangle():
+    o = 10
+    w = 7
+    print_top(o, w)
+    print_middle(o+2, w-6)
+    print_middle(o+1, w-4)
+    print_middle(o, w-2)
+    print_bottom(o, w)
+def main():
+    print_triangle()
+main()
 
