281998b528fa49529b372f595bb33822be0dc65a
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2018-11-17 17:21:26 -0500

    Add files via upload

---
+++ Test 2 study.py
@@ -0,0 +1,32 @@
+'''
+#Problem 1: short+long+short strings
+def combo_string(a, b):
+    if len(a) < len(b):
+        return a+b+a
+    else:
+        return b+a+b
+def main():
+    stringA = input("Enter string A: ")
+    stringB = input("Enter string B: ")
+    print(combo_string(stringA, stringB))
+main()
+'''
+#Problem 2: Find xyz in a string
+def xyz_there(string):
+    isXYZ = False
+    string = string.lower()
+    for i in range (len(string)):
+        if string[i:i+3] == "xyz":
+            isXYZ = True
+            print(string[i:i+3])
+        elif string[i:i+4] == ".xyz":
+            isXYZ = False
+            print(string[i:i+4])
+    if isXYZ == True:
+        return "XYZ is there"
+    else:
+        return "XYZ is not there"
+def main():
+    string = input("Enter a string: ")
+    print(xyz_there(string))
+main()
